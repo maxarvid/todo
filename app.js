c6c3fd1647be6de1ed0c5aa5108797d7c4d502e1
maxarvid <max.arvid.anderson@gmail.com>
2022-01-14 20:43:38 +0100

    WIP renders toDo array as 'li's

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let todos = [];
-todos = JSON.parse(localStorage.getItem("todos"));
-console.log(todos);
+let todos;
+todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const createTodo = (task) => {
   todos.push(task);
@@ -10,11 +9,24 @@ const createTodo = (task) => {
 let btn = document.querySelector("button");
 btn.onclick = () => {
   let input = document.getElementById("new-task").value;
+  console.log(todos);
   createTodo(input);
   alert(`I have been clicked, input is: ${input}`);
+  renderTodo();
 };
 
 const renderTodo = () => {
-  const ul = document.createElement('ul')
-  ul.setAttribute('id', 'todos')
-}
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "todos");
+
+  for (let i=0; i < todos.length; i++) {
+    let li = document.createElement("li")
+    li.setAttribute("key", i)
+    li.innerHTML += todos[i]
+    ul.appendChild(li)
+  }
+
+  document.getElementById("todo-list").appendChild(ul);
+
+
+};
